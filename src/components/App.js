d23f6c08e1c4639a12914c262e4ea7f058d3a1f0
federico <fgiannini95@gmail.com>
2025-09-23 09:51:59 +0100

    Add shared action

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,21 @@
 import * as React from "react";
-import { useSelector } from "react-redux";
+import { handleInitialData } from "../actions/shared";
+import { useSelector, useDispatch } from "react-redux";
+
 export default function App() {
+  const dispatch = useDispatch();
+  const loading = useSelector((state) => state.authedUser === null);
+
+  React.useEffect(() => {
+    dispatch(handleInitialData());
+  }, [dispatch]);
+
   const store = useSelector((store) => store);
   console.log("store: ", store);
 
-  return <div>Redux Polls</div>;
+  return (
+    <div className="container">
+      {loading === true ? null : <div>Redux Polls</div>}
+    </div>
+  );
 }
